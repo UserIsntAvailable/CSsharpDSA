6522f66d3a684f52faf0ece6c76ff99013b5159a
Angel Pineda <pinedap.angel@gmail.com>
2021-08-28 22:21:01 -0400

    Change(Graph): Implement 'Add' from IGraph

--- CSharpDSA/DataStructures/Graph.cs
+++ CSharpDSA/DataStructures/Graph.cs
@@ -2,16 +2,53 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using CSharpDSA.Algorithms;
 
 namespace CSharpDSA.DataStructures
 {
-    public class Graph<T> : IGraph<T>
+    public class Graph<T> : IGraph<T> where T : IComparable<T>
     {
         private readonly Dictionary<T, List<T>> _items;
 
         public Graph()
         {
             _items = new Dictionary<T, List<T>>();
+        }
+
+        public void Add(T item, T value)
+        {
+            if(item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            if(value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if(!_items.ContainsKey(item))
+            {
+                this.Add(item);
+            }
+
+            if(!_items.ContainsKey(value))
+            {
+                throw new KeyNotFoundException(
+                    "You can't add a value to a node when that value is yet added as a node"
+                );
+            }
+
+            var nodeList = _items[item];
+
+            var index = SearchAlgorithms.Instance.BinarySearch(nodeList, value);
+
+            nodeList.Insert(index, value);
+        }
+
+        public void Traverse(T from, T to, Action<T> action)
+        {
+            throw new NotImplementedException();
         }
 
         public int Count => _items.Count;
@@ -39,7 +76,7 @@ namespace CSharpDSA.DataStructures
 
             return _items.ContainsKey(item);
         }
-        
+
         public void CopyTo(T[] array, int arrayIndex) => Array.Copy(
             _items.Keys.ToArray(),
             0,
@@ -61,17 +98,5 @@ namespace CSharpDSA.DataStructures
         public IEnumerator<T> GetEnumerator() => _items.Keys.GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
-
-        #region NotImplemented
-        public void Add(T item, T value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Traverse(T from, T to, Action<T> action)
-        {
-            throw new NotImplementedException();
-        }
-        #endregion
     }
 }
