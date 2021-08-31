5d7881cfeb504fa80254923e4ae14c323a59c566
Angel Pineda <pinedap.angel@gmail.com>
2021-08-31 19:32:12 -0400

    Change(I/SearchAlgorithm): Implement 'BreadthFirstSearch' method.

--- CSharpDSA/Algorithms/ISearchAlgorithms.cs
+++ CSharpDSA/Algorithms/ISearchAlgorithms.cs
@@ -3,8 +3,27 @@ using System.Collections.Generic;
 
 namespace CSharpDSA.Algorithms
 {
+    /// <summary>
+    /// Declares 
+    /// </summary>
     public interface ISearchAlgorithms
     {
+        /// <summary>
+        /// Searches a value on a sorted list using a binary search algorithm.
+        /// </summary>
+        /// <param name="items">A sorted list of items</param>
+        /// <param name="value">The value to search for.</param>
+        /// <typeparam name="T">The type of elements in the list</typeparam>
         public int BinarySearch<T>(IList<T> items, T value) where T : IComparable<T>;
+
+        /// <summary>
+        /// Searches the path between a start point and a end point point using BFS algorithm.
+        /// </summary>
+        /// <param name="items">An adjacency list</param>
+        /// <param name="start">The start node</param>
+        /// <param name="end">The end node</param>
+        /// <typeparam name="T">The type of elements in the adjacency list</typeparam>
+        public IEnumerable<T> BreadthFirstSearch<T>(IDictionary<T, IList<T>> items, T start, T end)
+            where T : IComparable<T>;
     }
 }
--- CSharpDSA/Algorithms/SearchAlgorithms.cs
+++ CSharpDSA/Algorithms/SearchAlgorithms.cs
@@ -9,8 +9,18 @@ namespace CSharpDSA.Algorithms
         {
         }
 
-        public static ISearchAlgorithms Instance { get; } = new SearchAlgorithms();
+        public static SearchAlgorithms Instance { get; } = new SearchAlgorithms();
 
+        /// <summary>
+        /// Searches a value on a sorted list using a binary search algorithm. 
+        /// </summary>
+        /// <param name="items">A sorted list of items</param>
+        /// <param name="value">The value to search for.</param>
+        /// <typeparam name="T">The type of elements in the list</typeparam>
+        /// <returns>
+        /// If it is a positive <see cref="int"/>, it returns the index of the first instance of the value.
+        /// Else, it returns a negative value that if it is negated (~), shows the position where to insert the value to keep the list sorted. 
+        /// </returns>
         public int BinarySearch<T>(IList<T> items, T value) where T : IComparable<T>
         {
             var lIndex = 0;
@@ -34,6 +44,63 @@ namespace CSharpDSA.Algorithms
             }
 
             return~lIndex;
+        }
+
+        /// <summary>
+        /// Searches the path between a start point and a end point point using BFS algorithm.
+        /// </summary>
+        /// <param name="items">An adjacency list</param>
+        /// <param name="start">The start node</param>
+        /// <param name="end">The end node</param>
+        /// <typeparam name="T">The type of elements in the adjacency list</typeparam>
+        /// <returns>
+        /// If the path was found, it returns a <see cref="IEnumerable{T}"/> with each node ( not including start ) that connects start and end.
+        /// Else, it returns an empty <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public IEnumerable<T> BreadthFirstSearch<T>(IDictionary<T, IList<T>> items, T start, T end)
+            where T : IComparable<T>
+        {
+            Dictionary<T, T> trace = new() {{start, default(T)!},};
+            List<T> trackedNodes = new() {start,};
+
+            var i = 0;
+
+            do
+            {
+                var current = trackedNodes[i];
+
+                var edges = items[current];
+
+                foreach(var edge in edges)
+                {
+                    if(edge.CompareTo(end) == 0) goto END_NODE_FOUND;
+
+                    if(trackedNodes.Contains(edge)) continue;
+
+                    trackedNodes.Add(edge);
+                    trace.Add(edge, current);
+                    i++;
+                }
+            }
+            while(i != trackedNodes.Count - 1);
+
+            // If node 'end' wasn't connected to 'start'
+            return Array.Empty<T>();
+
+            END_NODE_FOUND:
+            List<T> path = new() {end,};
+
+            var parent = end;
+
+            while(parent.CompareTo(default(T)) != 0)
+            {
+                parent = trace[parent];
+                path.Add(parent);
+            }
+
+            path.Reverse();
+
+            return path;
         }
     }
 }
